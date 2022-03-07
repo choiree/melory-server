cba82a4a1eb20eaca83d9fda8aa770c73b9ecff9
choiree <riririgood@naver.com>
2022-03-08 07:11:31 +0900

    error handling

---
+++ constants.js
@@ -0,0 +1,8 @@
+exports.ERROR_MESSAGE = {
+  INVALID_USER: 'Invalid User',
+  INVALID_DOC: 'Invalid Document',
+  UNAUTHORIZED: 'unauthorized',
+  SERVER: 'Internal Server Error',
+  NOT_FOUND: 'Not Found',
+  FORBIDDEN: 'Forbidden',
+};
